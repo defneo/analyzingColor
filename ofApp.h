43cbe0a839939c4f4fb96feb86c4e9c6c82c0d10
Defne Onen <defneonen88@gmail.com>
2014-12-09 11:42:53 -0500

    Create ofApp.h

---
+++ ofApp.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ofMain.h"
+
+class histo: public ofBaseApp{
+
+	public:
+    
+		void setup();
+		void update();
+		void draw();
+
+        ofImage img;
+        int pxColors[2048]; //the array that will store all the colors
+        bool bChange;
+        void keyPressed  (int key);
+
+
+};
